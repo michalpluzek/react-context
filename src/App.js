6933026cbc28a413453d006ce89170279b409932
Michał Płużek <michalpluzek@gmail.com>
2021-12-03 10:54:08 +0100

    Cleanup code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Button from "./Button";
 import UserInfo from "./UserInfo";
 
-import { AppContext, defaultObject } from "./AppContext";
+import AppProvider from "./AppContext";
 
 import "./App.css";
 
-const App = () => {
-  const [isUserLogged, setIsUserLogged] = useState(defaultObject.isUserLogged);
-  const [isUserAdult, setIsUserAdult] = useState(true);
+const App = () => (
+  <div className="App">
+    <AppProvider>
+      <UserInfo />
+      <Button />
+    </AppProvider>
+  </div>
+);
 
-  const handleToggleStateIsLogged = () => {
-    setIsUserLogged((prevValue) => !prevValue);
-  };
-
-  const handleToggleStateIsAdult = () => {
-    setIsUserAdult((prevValue) => !prevValue);
-  };
-
-  return (
-    <div className="App">
-      <AppContext.Provider
-        value={{
-          isUserLogged,
-          toggleLoggedState: handleToggleStateIsLogged,
-        }}
-      >
-        <UserInfo />
-        <Button />
-      </AppContext.Provider>
-      <AppContext.Provider
-        value={{
-          isUserLogged: isUserAdult,
-          toggleLoggedState: handleToggleStateIsAdult,
-        }}
-      >
-        <UserInfo />
-        <Button />
-      </AppContext.Provider>
-    </div>
-  );
-};
 export default App;
--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+
 import { AppContext } from "./AppContext";
 
 const UserInfo = () => {
